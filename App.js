2e1f682284c44985a05a806d75e978c1b5fde9d5
Mughalzari <zaghal.mughal1999@gmail.com>
2024-06-09 14:15:23 +0500

    Updated React-native packages

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import MainNavigator from './registration/mainNavigator';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
+import FlashMessage from "react-native-flash-message";
+import { GlobalContextProvider } from "./context/GlobalContextProvider";
+import reducer, { initState } from "./context/reducer";
+import { GestureHandlerGestureEvent } from 'react-native-gesture-handler';
+import { SafeAreaView } from 'react-native';
 
-export default function App() {
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <SafeAreaView style={{ flex: 1 }}>
+      <GlobalContextProvider initialState={initState} reducer={reducer}>
+        <QueryClientProvider client={queryClient}>
+          <NavigationContainer>
+            <MainNavigator />
+          </NavigationContainer>
+        </QueryClientProvider>
+      </GlobalContextProvider>
+      <FlashMessage position="top" duration={5000} hideOnPress={true} />
+    </SafeAreaView>
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  );
+};
+
+export default App;
